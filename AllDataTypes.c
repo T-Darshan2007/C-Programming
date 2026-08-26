626ebdf5c1d219c9c7ccf9ee71a6cfb727adcccd
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-26 19:06:29 +0530

    Add files via upload

---
+++ AllDataTypes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 25;
+    float price = 500.567;
+    double pi = 3.14567893425;
+    char a = '$';
+    char str[] = "Supreme Atman";
+    printf("%d\n", age);
+    printf("%f\n", price);
+    printf("%lf\n", pi);
+    printf("%c\n", a);
+    printf("%s\n", str);
+    return 0;
+}
