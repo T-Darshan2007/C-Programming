279b7378d7996488a4c94e2c44696428e4ca31dc
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-09-07 21:24:41 +0530

    Add files via upload

---
+++ 01_reverse_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int left = 0;
+    int right = n - 1;
+
+    while(left < right)
+    {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+
+        left++;
+        right--;
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
