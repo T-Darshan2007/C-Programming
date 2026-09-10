f8c8e5e78962ae21eaae288dde820ca1722b347b
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-09-10 21:22:04 +0530

    Add files via upload

---
+++ boolean.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    bool c = 0;
+    if(c)
+    {
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
+    return 0;
+}
