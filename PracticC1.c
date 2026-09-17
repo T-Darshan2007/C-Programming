3a717d26e5e533d94120c9beb122eb09cb9c89f7
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-09-17 20:04:58 +0530

    Add files via upload

---
+++ PracticC1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[10];
+    /*str[0]="asd";
+    printf("%s", str[0]);
+    printf("%s", str);*/
+    str[0]='A';
+    printf("%s", str);
+    printf("%s", str[0]);
+    return 0;
+}
