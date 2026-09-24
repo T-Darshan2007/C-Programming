b45133955c5f230c315eceb378a5d382e53414da
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-09-24 20:37:50 +0530

    Add files via upload

---
+++ PracticeC2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void main()
+{
+    int day = 0;
+    switch (day)
+    {
+    case 1:
+        printf("Monday");
+        break;
+    case 2:
+        printf("Tuesday");
+        break;
+    case 3:
+        printf("Wednesday");
+        break;
+    case 4:
+        printf("Thursday");
+        break;
+    case 5:
+        printf("Friday");
+        break;
+    case 6:
+        printf("Saturday");
+        break;
+    case 7:
+        printf("Sunday");
+        break;
+    
+    default:
+        printf("Enter a number from(1-7)");
+        break;
+    }
+}
