78ab38ecc910128f2bf1e0500ca4a8fbb5e7ed14
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-09-24 20:51:23 +0530

    Add files via upload

---
+++ HappyBirthdayGreetings.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void happyBirthda();
+
+void main()
+{
+    happyBirthday();
+}
+
+void happyBirthday()
+{
+    printf("Wish You Happy Happy Birthday\n");
+    printf("Happy Birthday\n");
+    printf("Happy Birthday\n");
+    printf("To You\n");
+}
